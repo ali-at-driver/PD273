d66948509714ea5dcc58864c55da0a9c95b9ee17
alidalton <ali@dg-compute-res-6.sf.drivergrp.com>
2016-10-20 14:07:11 -0700

    small clean, tests, refactor FS

---
+++ PD274/src/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+    name='my-awesome-helloworld-script',
+    version='0.1',
+    scripts=['trimcount']
+)
--- enqueue_files.py
+++ enqueue_files.py
@@ -6,6 +6,8 @@ from config import Reports
 import cutadapt_wrapper
 from os import walk
 from dimerization_count import grep_report
+
+
 def get_filenames(path):
     f = []
     for (_, __, filenames) in walk(path):
@@ -23,21 +25,8 @@ def pair_files(path): #dict
     
     # there quite literally has to be a better way to do this
     for name in fnames:
-        # FIXME
-        name1 = './data/' + name[0] + 'R1' + name[1]
-        name2 = './data/' + name[0] + 'R2' + name[1]
+        # FIXME....formalize path 
+        name1 = path + name[0] + 'R1' + name[1]
+        name2 = path + name[0] + 'R2' + name[1]
         paired_fnames.append([name1, name2])
     return paired_fnames
-
-
-def mp_worker((file1, file2)):
-    output0, output1 = cutadapt_wrapper.run_paired(file1, file2)
-    dimerization_count.grep_report(outputs[0], outputs[1])
-def mp_handler(data):
-    p = multiprocessing.Pool(2)
-    p.map(mp_worker, data)
-
-#E144-T1-D1_S15_L004_I1_001.fastq.gz
-#if __name__=='__main__':
-#    data = pair_files('./data')
-#    mp_handler(data)
