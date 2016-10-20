be5ff0c62bffc18cc64cd81a82c2e6635db103b3
Ali Dalton <ali@dg-compute-res-6.sf.drivergrp.com>
2016-10-19 22:42:53 -0700

    threading

--- cutadapt_wrapper.py
+++ cutadapt_wrapper.py
@@ -1,34 +1,32 @@
+from config import Adapters, report_field
 from contextlib import contextmanager
-import tempfile # FIXME: immutable
-import subprocess
+from cStringIO import StringIO
 from cutadapt.scripts import cutadapt
 
 import re
 import os 
-
-from cStringIO import StringIO
+import subprocess
 import sys
+import tempfile # FIXME: immutable
 
+"""TODO
+general
+  Improve regex searches, report output 
+  TRY/Excepts
+  param overrides
+
+get_adapter()
+    #TODO: find a way to retrieve using cutadapt, forexample
+run()
+    # TODO improve redirect of standard output
+    # TODO diff log files
+    # TODO: implement param override
+
+grep_report():
+   # TODO: more sophisticated regex....
+   # TODO: MAKE THIS FUNCTION BETTER
 """
-residuals -- please ignore -- pre-git setup on cluster :) 
-class Constants:
-    read0 = 0
-    read1 = 1 
 
-p3 = subprocess.Popen(["cutadapt", "-a", adapters.adp, "-o", ""])#, stdin=p1.stdout, stdout=subprocess.PIPE)
-
-TODO: get opinion on static vars vs import 'config' file
-
-Will other modules in this project benefit from having a config file to read from later?
-
-"""
-
-class Adapters:
-    lab0 = "AATGATACGGCGACCACCGAGATCTACACXXXXXXXXACACTCTTTCCCTACACGACGCTCTTCCGATCT"
-    lab1= "GATCGGAAGAGCACACGTCTGAACTCCAGTCACXXXXXXXXATCTCGTATGCCGTCTTCTGCTTG"
-
-# FIXME
-REPORT_FIELD='Reads with adapters:'
 
 class Capturing(list):
     def __enter__(self):
@@ -55,9 +53,6 @@ def datapath(path):
 
 
 def get_adapter(infile, pre_adapter, read_num):
-    """ TODO: find a way to retrieve using cutadapt, forexample
-    """
-    print(infile)
     try: 
         p1 = subprocess.Popen(["zcat", infile], stdout=subprocess.PIPE)
         p2 = subprocess.Popen(["head", "-n", "1"], stdin=p1.stdout, stdout=subprocess.PIPE)
@@ -67,24 +62,17 @@ def get_adapter(infile, pre_adapter, read_num):
 
     # inserts is the read specific sequence that replaces the X*8 repeats in the lab adapter
     # more specific var name TBD
-    inserts = re.search('([ATCG]{5,8})', output) # TODO: more stringent regex
+    inserts = re.search(Adapters.insert_re, output) # TODO: more stringent regex
     
     if inserts:  # TODO: one or both? O|1
         p1.kill()
         p2.stdout.close()
-        return pre_adapter.replace("XXXXXXXX", inserts.group(read_num))  # TODO: FIXME
+        return pre_adapter.replace(Adapters.replace, inserts.group(read_num))  # TODO: FIXME
 
     return None
 
 
 def run(infile, outfile="/dev/null", params=None, lab_adapt_override=None):
-    """
-    single run read
-    params: override given
-    """
-    # TODO improve redirect of standard output
-    # TODO diff log files
-    # TODO: implement param override
     if type(params) is str:
         params = params.split()
 
@@ -94,31 +82,26 @@ def run(infile, outfile="/dev/null", params=None, lab_adapt_override=None):
     if lab_adapt_override is None:  # pythonic?
         pre_adapter = Adapters.lab0 if read_num == 0 else Adapters.lab1
     
-    params = ['-a', get_adapter(infile, pre_adapter, read_num)] #str(adapter)]] 
-
-    #with temporary_path(outfile) as tmp_fastaq: # IMPLEMENTED FOR TESTING, can remove 
-    params += ['-o', outfile ] # TODO parallelizable?
-    params += [datapath(infile)]
-        
-        # TODO: add infile2
+    # cutadapt can get cranky if these are too long
+    params = ['-a', get_adapter(infile, pre_adapter, read_num),
+              '-o', outfile, datapath(infile)]
+    # TODO parallelizable?
+    # TODO: add infile2
     with Capturing() as output:
         cutadapt.main(params)
         return output
         
 
 def run_paired(infile0, infile1, tmpfile=None, tmpfile1=None, params=None):
-    # TODO: implement param override 
+    # TODO: implement params AND param override 
     if type(params) is str:
         params = params.split()
-        #params += ['-o', p0, '-p', p1]
-        #params += [datapath(in0), datapath(in1)]
-    output0 = run(infile0)
-    
+
+    output0 = run(infile0)    
     output1 = run(infile1)
     return grep_report(output0, output1)
 
-# TODO: more sophisticated regex....
-# TODO: MAKE THIS FUNCTION BETTER
+
 def grep_report(report0, report1=None):
     found0 = filter(lambda x: 'Reads with adapters:' in x, report0)  # FIXME
     if report1:
@@ -130,5 +113,4 @@ def grep_report(report0, report1=None):
     
 
 if __name__=='__main__':
-    #print(grep_report(run("R1.fastq.gz")))
     print(run_paired("R1.fastq.gz", "R2.fastq.gz"))
